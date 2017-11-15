24cd4e9e68e75620940947ac0b27d07dce73b34b
Prashantpandey2398 <pprashant2398@gmail.com>
2017-11-16 00:32:13 +0530

    updated files

--- pythonProject/settings.py
+++ pythonProject/settings.py
@@ -127,7 +127,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 PROJECT_PATH = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'Web/static'),
 )   
