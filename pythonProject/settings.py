ff69814ac5e9bf2ab801c1700b2a50db5cd5d7e3
Prashantpandey2398 <pprashant2398@gmail.com>
2017-11-16 00:48:01 +0530

    updated files

--- pythonProject/settings.py
+++ pythonProject/settings.py
@@ -24,8 +24,9 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", 'mah=u^%*8-kod#g9nq2@ub!un3s&y+r8+cp
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", False)
+# DEBUG = os.getenv("DEBUG", True)
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 # Application definition
 
@@ -125,9 +126,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = '/static/'
+
 PROJECT_PATH = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'Web/static'),
+# )
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'Web/static'),
-)   
+    os.path.join(BASE_DIR, 'static'),
+)
