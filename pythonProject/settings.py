a5dcbdc53ea35d9ce6886968e8cf2aef2abac7f2
HITESHGOEL08 <hiteshgoel426@gmail.com>
2017-11-16 10:29:53 +0530

    test changes

--- pythonProject/settings.py
+++ pythonProject/settings.py
@@ -123,3 +123,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+PROJECT_PATH = os.path.dirname(os.path.abspath(__file__))
+STATICFILES_DIRS = (
+   os.path.join(BASE_DIR, 'static/'),
+)
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
